770151c94a8e3ccca8435aefe60c415ed18752bd
fiveseven-lambda <fiveseven.lambda@gmail.com>
2022-08-08 12:43:27 +0900

    1 から書き直し

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,19 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
-
-const dist = path.resolve(__dirname, "dist");
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
 
 module.exports = {
-  mode: "production",
-  entry: {
-    index: "./js/index.js"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js"
-  },
-  devServer: {
-    contentBase: dist,
-  },
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   plugins: [
-    new CopyPlugin([
-      path.resolve(__dirname, "static")
-    ]),
-
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
     }),
-  ]
-};
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, 'rust'),
+      outDir: path.resolve(__dirname, 'pkg'),
+    })
+  ],
+  experiments: {
+    asyncWebAssembly: true,
+  },
+}
