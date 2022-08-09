84564883a641583c35546e5a58da9b8a8cbabe3c
fiveseven-lambda <fiveseven.lambda@gmail.com>
2022-08-09 14:07:38 +0900

    play sound

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use web_sys::AudioBuffer;
 
 #[wasm_bindgen]
 extern {
@@ -8,4 +9,18 @@ extern {
 #[wasm_bindgen]
 pub fn hello() {
     alert("Hello!");
+}
+
+#[wasm_bindgen]
+pub fn write_sin(dest: &AudioBuffer) {
+    let sample_rate = dest.sample_rate();
+    let length = dest.length() as usize;
+    let mut buffer = vec![0.; length];
+    for i in 0..length {
+        let t = i as f32 / sample_rate as f32;
+        buffer[i] = (std::f32::consts::TAU * 440. * t).sin();
+    }
+    for i in 0..dest.number_of_channels() {
+        dest.copy_to_channel(&buffer, i as i32).unwrap();
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import('../pkg').then(module => {
-  module.hello();
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+  const buffer = audioCtx.createBuffer(2, audioCtx.sampleRate * 3, audioCtx.sampleRate);
+  module.write_sin(buffer);
+  const source = audioCtx.createBufferSource();
+  source.buffer = buffer;
+  source.connect(audioCtx.destination);
+  document.getElementById('play').onclick = () => source.start();
 })
